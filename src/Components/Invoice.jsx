2a2e1651cd4ddf42542d750feb3532a9b3b015a7
Rahul Biswas <rahul.b@techpax.in>
2023-06-23 18:09:03 +0530

    y

--- src/Components/Invoice.jsx
+++ src/Components/Invoice.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import logo from "../IMG/NavbarLogo.png";
 import "./Invoice.scss";
 import jsPDF from "jspdf";
